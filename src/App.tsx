669871f863d58499308c87de74375891dca47a50
GitHubMehdii <mehdi.mc60@gmail>
2020-12-04 22:42:54 +0100

    header: logo added

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import './App.css';
 import Header from './components/header/header';
 
 const App = () => {
-  return (
-    <>
-		<Header />
-		<div className="App mt-5">
-			Welcome
-		</div>
-    </>
-  );
-}
+	return (
+		<>
+			<Header />
+			<div className="App mt-5">Welcome</div>
+		</>
+	);
+};
 
 export default App;
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import "./header.css";
+import React from 'react';
+import './header.css';
+import logo from '../../assets/img/logo.png';
 
 function Header() {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light">
 			<div className="d-flex justify-content-between w-100">
 				<div>
-					<a className="navbar-brand" href="/">
-						MedCare 2.0
+					<a className="navbar-brand d-flex align-items-center" href="/">
+						<img src={logo} width="30" alt="logo" className="mr-2" />
+						<span className="c-green font-weight-bold">MedCare 2.0</span>
 					</a>
 				</div>
 				<div>
