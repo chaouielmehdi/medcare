fe3925bdc0303381467990c13306c4764eef8ecf
CHAHBOUN <mchahboun@dxc.com>
2020-12-08 21:56:27 +0100

    conflict resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import "./App.css";
 import Header from "./pages/header/header";
 import Footer from "./pages/footer/footer";
 import About from "./pages/about/about";
 import Home from "./pages/home/home";
-=======
-import './App.css';
-import Header from './pages/header/header';
-import About from './pages/about/about';
-import Home from './pages/home/home';
-import Doctors from './pages/search/doctors/doctors';
->>>>>>> 03ff8f9601570c63cfeda351b06b73f9c6091cd9
+import Doctors from "./pages/search/doctors/doctors";
 
 const App = () => {
 	return (
